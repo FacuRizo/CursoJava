bbde1eb67ba094a0c5c46901066db0b96c96121b
Facundo <114203255+FacuRizo@users.noreply.github.com>
2023-09-28 09:02:07 -0300

    Add files via upload

---
+++ Semana1/src/Ejercicio1b.java
@@ -0,0 +1,25 @@
+
+public class Ejercicio1b 
+{
+
+	public static void main(String[] args) 
+	{
+		int numeroInicio =1;
+		int numeroFinal = 25;
+		int counter = numeroInicio;
+		
+		while (counter<= numeroFinal)
+		{
+			int modulo =counter % 2;
+			if (modulo==0)
+			{				
+				System.out.println(counter);
+				
+			}
+		counter++;
+
+		}
+
+	}
+
+}
