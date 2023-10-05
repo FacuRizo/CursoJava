e1ff2c5fdfd10894dcd391cf941f3c05fd4c7951
Facundo <114203255+FacuRizo@users.noreply.github.com>
2023-10-05 15:48:30 -0300

    intro

---
+++ introduccion/src/introduccion/Ejercicio1.java
@@ -0,0 +1,21 @@
+package introduccion;
+
+
+public class Ejercicio1 
+{
+
+	public static void main(String[] args) 
+	{
+		int numeroInicio =1;
+		int numeroFinal = 25;
+		int counter = numeroInicio;
+		while (counter<= numeroFinal)
+		{
+		  System.out.println(counter);
+		  counter++;
+		  
+		}
+
+	}
+
+}
