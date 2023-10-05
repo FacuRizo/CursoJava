4f6f6cdf65579736a99eb897ba7351dfca0fdc89
FacuRizo <facurizo97@gmail.com>
2023-10-05 18:04:56 -0300

    tp1

---
+++ introduccion/src/introduccion/Ejercicio2d.java
@@ -0,0 +1,23 @@
+package introduccion;
+
+public class Ejercicio2d 
+{
+
+	public static void main(String[] args) 
+	{
+		float ingresos=490000;
+		int cantVehic=0;
+		int cantInmuebles=0;
+		int activosSocietarios=0;	
+		if ( (ingresos>=489083) || (cantVehic>=3) || 
+				(cantInmuebles>=3) || (activosSocietarios>=1) )
+		{
+			System.out.println("Es Categoria IV");
+		}
+		else 
+		{
+			System.out.println("Pertenece a otra categoria");
+		}
+	}
+
+}
